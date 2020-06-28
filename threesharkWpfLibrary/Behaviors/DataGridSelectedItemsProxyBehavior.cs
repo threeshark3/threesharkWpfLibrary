585981fcd6f4b764fdd473c438b772da8a6573e5
threeshark3 <threeshark3@gmail.com>
2020-06-28 23:33:16 +0900

    DataGridのSelectedItemsをBindingするビヘイビアを追加

---
+++ threesharkWpfLibrary/Behaviors/DataGridSelectedItemsProxyBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace threesharkWpfLibrary
+{
+    public class DataGridSelectedItemsProxyBehavior : Behavior<DataGrid>
+    {
+        public IEnumerable SelectedItemsProxy
+        {
+            get { return (IEnumerable)GetValue(SelectedItemsProxyProperty); }
+            set { SetValue(SelectedItemsProxyProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemsProxyProperty =
+            DependencyProperty.Register(nameof(SelectedItemsProxy), 
+                typeof(IEnumerable), 
+                typeof(DataGridSelectedItemsProxyBehavior), 
+                new FrameworkPropertyMetadata(Enumerable.Empty<object>()));
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.SelectedCellsChanged += OnSelectedCellsChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.SelectedCellsChanged -= OnSelectedCellsChanged;
+        }
+
+        private void OnSelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            this.SetCurrentValue(SelectedItemsProxyProperty,
+                AssociatedObject.SelectedCells
+                    .Where(x => x.IsValid)
+                    .Select(x => x.Item)
+                    .Distinct());
+        }
+    }
+}
